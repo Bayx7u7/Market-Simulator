fac90917b601d4bda117546e7b478d2550c32600
Bayx7u7 <trbayramali@gmail.com>
2026-08-09 14:35:11 +0300

    ilk yükleme

---
+++ Urun.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public partial class Urun : Node
+{
+	private bool _isDragging = false;
+	private Vector2 _dragOffset;
+
+
+	public override void _Ready()
+	{
+	}
+
+	public override void _Process(double delta)
+	{
+	}
+}
